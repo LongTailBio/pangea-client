99b0e64b39e2c84505391670ad8a1868df5e0a9e
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-02-23 09:45:58 -0500

    refactor: remove unused api functions

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -2,11 +2,7 @@ import { CancelTokenSource, AxiosRequestConfig } from 'axios';
 import useAxios, { Options } from "axios-hooks";
 
 import { API_BASE_URL, cancelableAxios } from './utils';
-import { OrganizationType } from './models/organization';
 import { UserType } from './models/user';
-import { SampleGroupType } from './models/sampleGroup';
-import { SampleType } from './models/sample';
-import { AnalysisResultType } from './models/analysisResult';
 
 export interface PaginatedResult<T> {
   count: number;
@@ -84,46 +80,6 @@ export const search = (query: string, source: CancelTokenSource) => {
     });
 };
 
-
-export const createOrganization = (name: string, adminEmail: string, source: CancelTokenSource) => {
-  const options: AxiosRequestConfig = {
-    url: `${API_BASE_URL}/organizations`,
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${window.localStorage.authToken}`
-    },
-    data: {
-      name,
-    },
-  };
-
-  return cancelableAxios(options, source);
-};
-
-export const getOrganization = (uuid: string, source: CancelTokenSource) => {
-  const options: AxiosRequestConfig = {
-    url: `${API_BASE_URL}/organizations/${uuid}`,
-    method: 'get',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${window.localStorage.authToken}`
-    }
-  };
-
-  return cancelableAxios(options, source)
-    .then(res => {
-      const organization: OrganizationType = {
-        uuid: res.data.organization.uuid,
-        name: res.data.organization.name,
-        created_at: res.data.organization.created_at,
-        updated_at: res.data.organization.updated_at,
-      }
-
-      return organization;
-    });
-};
-
 export const getUser = (uuid: string, source: CancelTokenSource) => {
   const options: AxiosRequestConfig = {
     url: `${API_BASE_URL}/users/${uuid}`,
@@ -156,118 +112,4 @@ export const getUserStatus = (source: CancelTokenSource) => {
 
   return cancelableAxios(options, source)
     .then(res => res.data.data);
-};
-
-export const getSampleGroup = (uuid: string, source: CancelTokenSource) => {
-  const options: AxiosRequestConfig = {
-    url: `${API_BASE_URL}/sample_groups/${uuid}`,
-    method: 'get',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${window.localStorage.authToken}`
-    },
-  };
-
-  return cancelableAxios(options, source)
-    .then(res => {
-      const sampleGroup: SampleGroupType = {
-        uuid: res.data.sample_group.uuid,
-        organization: res.data.sample_group.organization_id,
-        name: res.data.sample_group.name,
-        description: res.data.sample_group.description,
-        is_public: res.data.sample_group.is_public,
-        theme: res.data.sample_group.theme,
-        created_at: res.data.sample_group.created_at,
-        updated_at: res.data.organization.updated_at,
-      };
-      return sampleGroup;
-    });
-};
-
-export const getSampleGroupSamples = (uuid: string, source: CancelTokenSource) => {
-  const options: AxiosRequestConfig = {
-    url: `${API_BASE_URL}/sample_groups/${uuid}/samples`,
-    method: 'get',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${window.localStorage.authToken}`
-    },
-  };
-
-  return cancelableAxios(options, source)
-    .then(res => {
-      const rawSamples = res.data.data.samples;
-
-      const samples: SampleType[] = rawSamples.map((rawSample: any) => {
-        return {
-          uuid: rawSample.uuid,
-          name: rawSample.name,
-          analysisResultUuid: rawSample.analysis_result_uuid,
-          metadata: rawSample.metadata,
-        };
-      });
-      return samples;
-    });
-};
-
-export const getSample = (uuid: string, source: CancelTokenSource) => {
-  const options: AxiosRequestConfig = {
-    url: `${API_BASE_URL}/samples/${uuid}`,
-    method: 'get',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${window.localStorage.authToken}`
-    },
-  };
-
-  return cancelableAxios(options, source)
-    .then(res => {
-      const sample: SampleType = {
-          uuid: res.data.sample.uuid,
-          library: res.data.sample.library_id,
-          name: res.data.sample.name,
-          metadata: res.data.sample_metadata,
-          created_at: res.data.sample.created_at,
-          updated_at: res.data.sample.updated_at,
-      };
-      return sample;
-    });
-};
-
-export const getAnalysisResult = (uuid: string, source: CancelTokenSource) => {
-  const options: AxiosRequestConfig = {
-    url: `${API_BASE_URL}/analysis_results/${uuid}`,
-    method: 'get',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${window.localStorage.authToken}`
-    },
-  };
-
-  return cancelableAxios(options, source)
-    .then(res => {
-      const ar: AnalysisResultType = {
-          uuid: res.data.analysis_result.uuid,
-          module_name: res.data.analysis_result.module_name,
-          replicate: res.data.analysis_result.replicate,
-          created_at: res.data.analysis_result.created_at,
-          updated_at: res.data.analysis_result.updated_at,
-          sample: res.data.analysis_result.sample,
-          sample_group: res.data.analysis_result.sample_group,
-      };
-      return ar;
-    });
-};
-export const getAnalysisResults = (uuid: string, source: CancelTokenSource) => {
-  const options: AxiosRequestConfig = {
-    url: `${API_BASE_URL}/analysis_results/${uuid}`,
-    method: 'get',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Token ${window.localStorage.authToken}`
-    },
-  };
-
-  return cancelableAxios(options, source)
-    .then(res => res.data.data.analysis_result as AnalysisResultType);
 };
