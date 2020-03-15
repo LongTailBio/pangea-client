bf733a1b14c33b1bddf5afe0fc3a9c312f43ecdc
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2020-03-15 17:25:46 -0400

    feat(covid19): add empty covid19 page

---
+++ src/contrib/covid19/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Covid19 = () => {
+  return <h1>COVID-19</h1>;
+};
+
+export default Covid19;
--- src/contrib/router.tsx
+++ src/contrib/router.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route, RouteComponentProps } from "react-router-dom";
 
 import TaxaSearch from "./taxa-search";
+import Covid19 from "./covid19";
 
 export const ContribRouter = (props: RouteComponentProps) => {
   const { path } = props.match;
   return (
     <Switch>
       <Route path={`${path}/taxa-search`} component={TaxaSearch} />
+      <Route path={`${path}/covid-19`} component={Covid19} />
     </Switch>
   );
 };
